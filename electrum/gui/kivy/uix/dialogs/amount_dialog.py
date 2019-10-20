2d9f98247528960501e7ef0758f4c237cc130203
flurbos <31379149+Flurbos@users.noreply.github.com>
2019-10-20 19:31:13 +1100

    Fix .apk Send

--- electrum/gui/kivy/uix/dialogs/amount_dialog.py
+++ electrum/gui/kivy/uix/dialogs/amount_dialog.py
@@ -112,7 +112,7 @@ Builder.load_string('''
                     height: '48dp'
                     text: _('OK')
                     on_release:
-                        root.callback(btc.text if kb.amount else '')
+                        root.callback(uno.text if kb.amount else '')
                         popup.dismiss()
 ''')
 
